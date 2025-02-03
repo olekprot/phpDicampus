e1dd1a31d5dfc459e7576f279b5fa543f3cfce79
olekprot <olekprot@gmail.com>
2025-02-03 14:18:39 +0100

    foreach

--- dia4/coches.php
+++ dia4/coches.php
@@ -1,8 +1,53 @@
-<?php
-    include 'coches.json';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            text-align: center;
+            padding-top: 25px
+        }
+        ul {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+        li {
+            background-color: tan;
+            padding: 10px;
+            margin: 10px;
+            list-style: none;
+            flex: 1 1 250px;
+            border-radius: 10px;
+        }
+        p {
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Lista de los coches</h1>
+    <ul>
+    <?php
     
     $datos = json_decode(file_get_contents('coches.json'), true);
-    echo '<pre>';
-    print_r($datos);
-    echo '</pre>';
+
+    foreach($datos as $car) {
+        echo '<br>';
+        echo '<li>';
+        echo '<p>Marca: ' . $car['marca'] . '</p>';
+        echo '<p>Modelo: ' . $car['modelo'] . '</p>';
+        echo '<p>Cilindrada: ' . $car['cilindrada'] . '</p>';
+        echo '<p>Velocidad Máxima: ' . $car['velocidad_maxima'] . '</p>';
+        echo '</li>';
+    }
+
 ?>
+    </ul>
+
+</body>
+</html>
+
+
